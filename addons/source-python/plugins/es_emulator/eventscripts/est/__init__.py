a1f9e44938ed26a3cd8d4832aa21a4c6eb14b4c8
spitice <spitice@outlook.jp>
2025-04-16 21:26:42 +0900

    Add polyfill for est module

---
+++ addons/source-python/plugins/es_emulator/eventscripts/est/__init__.py
@@ -0,0 +1,21 @@
+
+import es
+import playerlib
+
+def health(userid, iHealth):
+    playerlib.getPlayer(userid).setHealth(iHealth)
+
+def speed(userid, multiplier):
+    playerlib.getPlayer(userid).setSpeed(multiplier)
+
+def noclip(userid, isEnabled):
+    playerlib.getPlayer(userid).noclip(isEnabled)
+
+def freeze(userid, isFrozen):
+    playerlib.getPlayer(userid).freeze(isFrozen)
+
+def give(userid, weaponName):
+    es.give(userid, weaponName)
+
+def spawn(userid, unknownArg = 0):
+    es.spawnplayer(userid)
