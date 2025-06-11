824cd90cb3d2053e9fa01f82a228c9f919abab15
spitice <spitice@outlook.jp>
2025-06-12 00:32:31 +0900

    Add dummy for spe extension module

---
+++ addons/source-python/plugins/es_emulator/eventscripts/spe/__init__.py
@@ -0,0 +1,9 @@
+
+#
+# Placeholder for spe extension module
+#
+# Just for avoiding errors caused by `import spe` written in legacy modules.
+#
+# Remove this directory after you install the actual spe module
+# https://github.com/Ayuto/source-python-extensions
+#
